918681231b8e2cdbeeadc4def7242a1ef8b251bf
colledwards <colledwards@expediagroup.com>
2023-02-23 13:41:02 -0800

    modified Runway class

--- Runway.js
+++ Runway.js
@@ -4,15 +4,26 @@ class Runway {
 
     name;
 
-    constructor(name) {
-        this.name = name
+    constructor(name){
+        if(!name) throw new Error('Missing name argument');
+        this.name = name;
     }
     
-    add(plane) {
-
+    add(plane){
         if (Runway.planes.length>Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
-            throw new Error ("runways at full capacity!")
+            throw new Error ("runways at full capacity!");
         }
-        Runway.planes.push(plane)
+        Runway.planes.push(plane);
+    }
+
+    remove(plane){
+        if(!Runway.planes.includes(plane)) throw new Error("plane not in runway")
+        Runway.planes.splice(Runway.planes.indexOf(plane), 1)
+    }
+
+    removeAll(){
+        Runway.planes = [];
     }
 }
+
+module.exports = Runway;
